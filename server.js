a838778a300ba4c725a76eb9a04db020e85ac1b8
AswathMurugan <aswathmurugan20@gmail.com>
2016-11-08 09:38:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 var express = require('express');
+var app = express();
+app.get('/',function(request,response){
+	response.send("hello");
+});
+
+
 var server = app.listen(8000, function () {
   var port = server.address().port
 
